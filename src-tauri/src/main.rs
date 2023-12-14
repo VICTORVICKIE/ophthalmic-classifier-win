ae60cd4e7e798ad5dbaa4e823bb19bf9b0b7a270
VIGNESH KUMAR <45727291+VICTORVICKIE@users.noreply.github.com>
2023-12-14 20:11:23 +0530

    feat: verbose logging

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 #![allow(dead_code, unused)]
+use chrono::Utc;
+use chrono_tz::Asia::Kolkata;
 
 use std::{
     collections::HashMap,
-    fs::{File, OpenOptions},
+    error::Error,
+    fs::{self, File, OpenOptions},
     io::{self, Write},
     mem,
-    path::PathBuf,
+    path::{Path, PathBuf},
+    sync::Arc,
 };
 
 use serde::{Deserialize, Serialize};
@@ -42,78 +46,144 @@ impl<'a> Response<'a> {
             message: msg,
         };
     }
-    fn to_json(&self) -> String {
-        serde_json::to_string(self).expect("Response -> String")
+}
+
+fn emit_prediction(app: &tauri::AppHandle, event: &CommandEvent) {
+    let window = app
+        .get_window("main")
+        .ok_or_else(|| log(&app, &format!("Unable tot get main window")))
+        .unwrap();
+    match event {
+        CommandEvent::Stdout(res) => {
+            log(app, &format!("[RESPONSE] {}", res.trim()));
+            window
+                .emit("prediction", Some(res))
+                .map_err(|err| {
+                    log(
+                        &app,
+                        &format!("[ERROR] Failed to emit prediction event: {}", err),
+                    )
+                })
+                .unwrap();
+        }
+        CommandEvent::Stderr(tf_log) => log(app, &format!("[TF LOG] {}", tf_log.trim())),
+        CommandEvent::Error(e) => log(app, &format!("[ERROR] {}\n{}", e.trim(), "-".repeat(100))),
+        CommandEvent::Terminated(signal) => log(
+            app,
+            &format!("[TERMINATED] {:?}\n{}", signal, "-".repeat(100)),
+        ),
+        _ => log(
+            app,
+            &format!("[ERROR] Unknown Event Encountered\n{}", "-".repeat(100)),
+        ),
     }
 }
-fn log(file: &str, msg: &str) {
-    let mut log_file = match OpenOptions::new().append(true).create(true).open(file) {
+
+#[derive(Debug, Clone)]
+struct Resource(Arc<str>);
+
+impl Resource {
+    fn new(app: &tauri::AppHandle, p: &str) -> Result<Self, Box<dyn Error>> {
+        let path = app
+            .path_resolver()
+            .resource_dir()
+            .and_then(|p| dunce::canonicalize(p).ok());
+
+        let path = match path.as_ref().and_then(|f| f.to_str()) {
+            Some(p) => p,
+            None => return Err(format!("[ERROR] Unable to resolve path: {}", p).into()),
+        };
+
+        let path: PathBuf = [path, p].iter().collect();
+        let path = match path.to_str() {
+            Some(p) => p,
+            None => return Err(format!("[ERROR] Unable to resolve path: {}", p).into()),
+        };
+        return Ok(Self(Arc::from(path)));
+    }
+
+    fn path(&self) -> &str {
+        &self.0
+    }
+
+    fn join(&mut self, p: &str) -> Result<Self, Box<dyn Error>> {
+        let path: PathBuf = [self.path(), p].iter().collect();
+        let path = match path.to_str() {
+            Some(p) => p,
+            None => return Err(format!("[ERROR] Unable to join path: {}", p).into()),
+        };
+        return Ok(Self(Arc::from(path)));
+    }
+}
+
+fn log(app: &tauri::AppHandle, msg: &str) {
+    let mut log_dir = match Resource::new(&app, "logs") {
+        Ok(l) => l,
+        Err(_) => return,
+    };
+    if !Path::new(&log_dir.path()).exists() {
+        fs::create_dir(log_dir.path());
+    }
+
+    let ist = Utc::now().with_timezone(&Kolkata);
+
+    let date = &ist.format("ophthalmic.classifier.%Y%m%d.log").to_string();
+    let time = &ist.format("[%Y-%m-%d %H:%M:%S]").to_string();
+
+    let log = match log_dir.join(date) {
+        Ok(l) => l,
+        Err(_) => return,
+    };
+
+    let mut f = match OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(log.path())
+    {
         Ok(f) => f,
         Err(_) => return,
     };
 
-    writeln!(log_file, "{}", msg);
-}
-
-fn emit_prediction(window: &tauri::Window, event: &CommandEvent) {
-    let log_dir = window.app_handle().path_resolver().app_log_dir();
-    let log_dir = log_dir
-        .as_ref()
-        .and_then(|f| f.to_str())
-        .expect("App Log Dir");
-    println!("{}", log_dir);
-
-    match event {
-        CommandEvent::Stdout(res) => {
-            println!("{}", res);
-            window
-                .emit("prediction", Some(res))
-                .expect("failed to emit prediction event");
-        }
-        CommandEvent::Stderr(tf_log) => {
-            println!("{}", tf_log);
-        }
-        CommandEvent::Error(e) => {
-            println!("{}", e);
-        }
-        CommandEvent::Terminated(signal) => {
-            println!("{:?}", signal);
-        }
-        _ => {}
+    match writeln!(f, "{} {}", time, msg) {
+        Ok(_) => return,
+        Err(_) => return,
     }
-}
-
-fn emit_failure(window: &tauri::Window, msg: &str) {
-    window
-        .emit("prediction", Some(Response::failure(msg).to_json()))
-        .expect("failed to emit prediction event");
 }
 
 #[tauri::command]
 fn predictor<'a>(app: tauri::AppHandle, model: &'a str, file: &'a str) {
-    let window = app.get_window("main").expect("Main Window");
-    let env = HashMap::from([("TF_CPP_MIN_LOG_LEVEL".to_owned(), "3".to_owned())]);
-
-    let model_path = app
-        .path_resolver()
-        .resolve_resource("models")
-        .and_then(|p| dunce::canonicalize(p).ok());
-
-    let model_dir = model_path
-        .as_ref()
-        .and_then(|f| f.to_str())
-        .expect("$RESOURCE/model");
+    let window = app
+        .get_window("main")
+        .ok_or_else(|| log(&app, &format!("[ERROR] Unable tot get main window")))
+        .unwrap();
+    let model_dir = Resource::new(&app, "models")
+        .map_err(|err| {
+            log(
+                &app,
+                &format!(
+                    "[ERROR] Unable to resolve models directory/folder!: {}",
+                    err
+                ),
+            )
+        })
+        .unwrap();
 
     let (mut rx, _) = Command::new_sidecar("oct-tf")
-        .expect("failed to create `oct-tf` binary command")
-        .args(["-d", model_dir, "-n", model, "-i", file])
-        // .envs(env)
+        .map_err(|err| {
+            log(
+                &app,
+                &format!("[ERROR] Failed to create `oct-tf` binary command: {}", err),
+            )
+        })
+        .unwrap()
+        .args(["-d", model_dir.path(), "-n", model, "-i", file])
         .spawn()
-        .expect("Failed to spawn sidecar");
+        .map_err(|err| log(&app, &format!("[ERROR] Failed to spawn sidecar: {}", err)))
+        .unwrap();
 
     tauri::async_runtime::spawn(async move {
         while let Some(event) = rx.recv().await {
-            emit_prediction(&window, &event)
+            emit_prediction(&app, &event)
         }
     });
 }
@@ -122,5 +192,5 @@ fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![predictor])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("Error while running tauri application");
 }
